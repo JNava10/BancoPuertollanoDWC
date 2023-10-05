9b83cb251dd785a14e66a6bc69f72ea5962bb6fc
Laura Pedraza <laurampego@gmail.com>
2023-10-05 14:05:45 +0200

    implementación botón modificar datos

--- app.js
+++ app.js
@@ -3,7 +3,33 @@ let nombreInput = document.getElementById('nombre');
 let apellido1Input = document.getElementById('apellido1');
 let apellido2Input =document.getElementById('apellido2');
 let nacionalidadInput = document.getElementById('nacionalidad');
+const guardarInput = document.getElementById('guardar');
 const mensajeOcultoInput = document.getElementById('mensajeOculto');
+
+class Persona {
+  static contador = 0; // Contador para generar ids automáticos
+
+  constructor(nombre, apellido1, apellido2, nacionalidad) {
+    this.id = ++Persona.contador;
+    this.nombre = nombre;
+    this.apellido1 = apellido1;
+    this.apellido2 = apellido2;
+    this.nacionalidad = nacionalidad;
+  }
+}
+
+var persona =
+
+function cargarDatos(){
+    persona = new Persona('Persona','Apellido','Apellido', 'Frances' )
+}
+
+function navegar(){
+    var co = JSON.stringify(persona);
+    localStorage.setItem("persona",co)
+    window.location.href='datos.html'
+}
+
 
 function cargarDatos(){
     let menu = document.getElementById('menu').innerHTML;
@@ -21,16 +47,29 @@ function cargarCabecera(dest){
 
 function validarCampos() {
     if (!campoEsValido(nombreInput.value, /A-Z[a-z]{3,20}/)) {
-        mensajeOcultoInput.value += "Nombre no valido";
+        mensajeOcultoInput.innerHTML += "Nombre no valido<br>";
     } else if (!campoEsValido(apellido1Input.value, /A-Z[a-z]{2,19}/)) {
-        mensajeOcultoInput.value += "Primer apellido no valido";
+        mensajeOcultoInput.innerHTML += "Primer apellido no valido<br>";
     } else if (!campoEsValido(apellido2Input.value, /A-Z[a-z]{2,19}/)) {
-        mensajeOcultoInput.value += "Segundo apellido no valido";
+        mensajeOcultoInput.innerHTML += "Segundo apellido no valido<br>";
     } else if (!campoEsValido(nacionalidadErrorCampo.value, /[a-z]{3,15}/)) {
-        mensajeOcultoInput.value += "Nacionalidad no valida";
+        mensajeOcultoInput.innerHTML += "Nacionalidad no valida";
     }
 }
 
 function campoEsValido(valor, regexp) {
     return regexp.test(valor);
 }
+
+guardarInput.addEventListener('click', function(){
+
+    if (mensajeOcultoInput.innerHTML === ""){
+        if (persona === null){
+            persona = Persona(nombreInput.value, apellido1Input.value, apellido2Input.value, nacionalidadInput.value)
+            mensajeOcultoInput.style.color = 'green'
+            mensajeOcultoInput.innerHTML = "Guardado los datos correctamente"
+        }
+    }
+})
+
+
